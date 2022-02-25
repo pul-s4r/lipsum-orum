8df529a41c215cd14fd92caae79c4ae8202848eb
Gary N <nguygary@gmail.com>
2022-02-26 01:09:00 +1100

    fix: separated name inputs

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -11,20 +11,50 @@ const LoginForm = () => {
       <div class="container">
         <form>
           <h1>Create account</h1>
-          <label for="fullname">
-            <h4>Full Name</h4>
+          <label for="firstName">
+            <h4>Full name</h4>
           </label>
-          <input type="text" id="fullname" name="fullname" />
+          <div className="fullName">
+            <input
+              placeholder="First name"
+              type="text"
+              id="firstName"
+              name="firstName"
+              required
+            />
+            <input
+              placeholder="Last name"
+              type="text"
+              id="lastName"
+              name="lastName"
+              required
+            />
+          </div>
           <label for="email">
             <h4>Email</h4>
           </label>
-          <input type="email" id="email" name="email" />
+          <input
+            placeholder="Type your email"
+            type="email"
+            id="email"
+            name="email"
+            required
+          />
           <label for="pw">
             <h4>Password</h4>
           </label>
-          <input type="password" id="lname" name="password" />
+          <input
+            placeholder="Password"
+            type="password"
+            id="lname"
+            name="password"
+            required
+          />
           <input type="submit" value="Sign Up" />
         </form>
+        <p>
+          Already have an account? <a href="url">Log in here</a>
+        </p>
       </div>
       <div className="info">
         <h1>Lorem Ipsum</h1>
