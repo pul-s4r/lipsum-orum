78c9cb3ebe1a441131a2ec9f03475fcb710dc7dd
Tom Tak <y0ung-gil@hanmail.net>
2022-02-26 23:37:22 +1100

    Comment component debugging done :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,47 @@
-
 // import FullPageProfile from './components/FullPageProfile'
-import LargeEventModal from './components/LargeEventModal/LargeEventModal';
+import LargeEventModal from "./components/LargeEventModal/LargeEventModal";
 // import logo from './logo.svg';
 //import './App.css';
 
 function App() {
-  const people = [
+  const events = [
     {
       eventName: "Ken's 31st Birthday",
-      pictureUrl: 'https://images.unsplash.com/photo-1533294160622-d5fece3e080d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+      pictureUrl:
+        "https://images.unsplash.com/photo-1533294160622-d5fece3e080d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
       eventDate: "1 Jan 1992",
       author: "Christine Powell",
       createDate: "1 Jan, 2022",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec, euismod iaculis elit morbi fermentum placerat sed iaculis tempus. Ultricies ornare in viverra auctor leo turpis enim, leo at. Nunc ultrices dictum integer sed.",
-      comments: [{sender: "Marie Powell",
-            profileUrl: "https://images.unsplash.com/photo-1531123414780-f74242c2b052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-        description: "Such a fun night!! We should do it again!"},
-        {sender: "Benjamin Powell",
-        profileUrl: "https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzE2fHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-        description: "Wonderful night, I ate so much cake xD"},
-        {sender: "Marie Powell",
-        profileUrl: "",
-        description: "Such a fun night!! We should do it again!"}],
-      tags: ["Birthday", "Party", "Ken", "2022"]
-      }
-  ]
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nec, euismod iaculis elit morbi fermentum placerat sed iaculis tempus. Ultricies ornare in viverra auctor leo turpis enim, leo at. Nunc ultrices dictum integer sed.",
+      comments: [
+        {
+          id: 1,
+          sender: "Marie Powell",
+          profileUrl:
+            "https://images.unsplash.com/photo-1531123414780-f74242c2b052?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+          description: "Such a fun night!! We should do it again!",
+        },
+        {
+          id: 2,
+          sender: "Benjamin Powell",
+          profileUrl:
+            "https://images.unsplash.com/photo-1504257432389-52343af06ae3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzE2fHxwZW9wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+          description: "Wonderful night, I ate so much cake xD",
+        },
+        {
+          id: 3,
+          sender: "Marie Powell",
+          profileUrl: "",
+          description: "Such a fun night!! We should do it again!",
+        },
+      ],
+      tags: ["Birthday", "Party", "Ken", "2022"],
+    },
+  ];
   return (
     <div className="App">
-      <LargeEventModal element={people}></LargeEventModal>
+      <LargeEventModal event={events[0]}></LargeEventModal>
     </div>
   );
 }
--- client/src/components/LargeEventModal/LargeEventModal.js
+++ client/src/components/LargeEventModal/LargeEventModal.js
@@ -1,68 +1,38 @@
-import './LargeEventModal.css';
-import Comments from '../Comments/Comments';
-import Comment from '../Comment/Comment';
-import Tags from '../Tags/Tags';
+import "./LargeEventModal.css";
+import Comment from "../Comment/Comment";
+import Tags from "../Tags/Tags";
 
 function LargeEventModal(props) {
-    // const profilePic = props.element[0].pictureUrl;
-    // const arrayOfObjects = props.element[0].comments;
-    // console.log(arrayOfObjects)
-    // const imageLink = `../images/${profilePic}`;
+  const comments = props.event.comments.map((comment) => {
+    return <Comment key={comment.id} {...comment} />;
+  });
 
-    // const arrayOfObjects = [
-    //     { sender: "Americano", message: "Single", profileUrl:"test" },
-    //     { sender: "Espresso", message: "Single", profileUrl:"test2" },
-    //   ];
-
-      const result = props.element[0].comments.map((eventObj) => (
-         eventObj.comments
-      ))
-console.log(result)
-    return (
-        <div className="content">
-            <div className="header">
-                <img className="photo" src="https://images.unsplash.com/photo-1533294160622-d5fece3e080d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
-                {/* <img src={require({imageLink})} /> */}
-                <div className="header-info">
-                    <p className="name">{props.element[0].eventName}</p>
-                    <p className="date">{props.element[0].eventDate}</p>
-                    <p className="subheading author">{props.element[0].author}</p>
-                    <p className="body createDate">{props.element[0].createDate}</p>
-                    <p className="body">{props.element[0].description}</p>
-                    
-                </div>
-            </div>
-                <div className="row">
-                <div className="column left" >
-
-                    <h3 className="subheading">Comments</h3>
-                    {/* {arrayOfObjects.map(item => <p item={item}></p>)} */}
-
-                {/* <p>{result}</p> */}
-                <Comments comment={result}></Comments>
-
-
-                    {/* <p>{arrayOfObjects.comments}</p> */}
-                    {/* {arrayOfObjects.map(item => <p>{item}</p>)} */}
-                    {/* {props.element[0].comments.map(item => <Comment item={item}></Comment>)} */}
-                    {/* <Comment comment={props.element[0].comments}></Comment>
-                    <Comment comment={props.element[0].comments}></Comment>
-                    <Comment comment={props.element[0].comments}></Comment> */}
-                </div>
-                <div className="column right" >
-                   
-                    <h3 className="subheading">Tags</h3>
-                    <Tags tags={props.element[0].tags}></Tags>
-                </div>
-                </div>
-
-
-
-            
+  return (
+    <div className="content">
+      <div className="header">
+        <img
+          className="photo"
+          src="https://images.unsplash.com/photo-1533294160622-d5fece3e080d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        />
+        <div className="header-info">
+          <p className="name">{props.event.eventName}</p>
+          <p className="date">{props.event.eventDate}</p>
+          <p className="subheading author">{props.event.author}</p>
+          <p className="body createDate">{props.event.createDate}</p>
+          <p className="body">{props.event.description}</p>
         </div>
-
-
-
-    )
+      </div>
+      <div className="row">
+        <div className="column left">
+          <h3 className="subheading">Comments</h3>
+          {comments}
+        </div>
+        <div className="column right">
+          <h3 className="subheading">Tags</h3>
+          <Tags tags={props.event.tags}></Tags>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default LargeEventModal;
