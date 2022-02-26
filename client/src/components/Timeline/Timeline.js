f6a38a5a5bafeafd8e98b2ced9c7efa85e2064f1
Tom Tak <y0ung-gil@hanmail.net>
2022-02-26 19:57:22 +1100

    Timeline Event Card onGoing:

--- client/src/components/Timeline/Timeline.js
+++ client/src/components/Timeline/Timeline.js
@@ -5,23 +5,33 @@ import TimelineHeader from "./TimelineHeader";
 import TimelineEventCard from "./TimelineEventCard";
 
 import dummyImg from "../../assets/dummy.png";
+import dummyImg2 from "../../assets/dummy2.jpg";
+import dummyImg3 from "../../assets/dummy3.jpg";
 
 const Timeline = (props) => {
   const items = [
     {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-      media: {
-        type: "IMAGE",
-        source: {
-          url: "http://someurl/image.jpg",
-        },
-      },
+      id: 1,
+      title: "Watching Sunset",
+      date: new Date(2001, 0, 1),
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper nisi ac nunc vehicula, in convallis risus consequat. Phasellus semper lacus nisl, at pharetra felis dignissim sed. Nunc et orci ac mi malesuada placerat. Nunc ultrices ante eu velit rutrum, vel laoreet metus varius. Quisque et lectus quam. In accumsan, sem id condimentum lobortis, elit urna porttitor elit, id lacinia dolor orci a tellus. Mauris in tortor ac justo lacinia tincidunt at in mauris. Etiam hendrerit neque non libero vehicula varius.",
+      imgSrc: dummyImg,
+    },
+    {
+      id: 2,
+      title: "Watching Sunset",
+      date: new Date(2003, 2, 1),
+
+      description: "Watched a sunset on the rooftop",
+      imgSrc: dummyImg2,
+    },
+    {
+      id: 3,
+      title: "Watching Sunset",
+      date: new Date(2005, 7, 1),
+      description: "Watched a sunset on the rooftop",
+      imgSrc: dummyImg3,
     },
   ];
 
@@ -33,12 +43,22 @@ const Timeline = (props) => {
         theme={{
           primary: "#1dabfd",
           secondary: "#FFB44F",
+          cardBgColor: "#4a71ff",
         }}
-        cardWidth="330"
+        cardWidth="230"
         lineWidth="3"
       >
-        <TimelineEventCard imgUrl={dummyImg} />
-        <TimelineEventCard />
+        {items.map((item) => {
+          return (
+            <TimelineEventCard
+              key={item.id}
+              imgUrl={item.imgSrc}
+              title={item.title}
+              date={item.date}
+              description={item.description}
+            />
+          );
+        })}
       </Chrono>
     </div>
   );
--- client/src/components/Timeline/TimelineEventCard.js
+++ client/src/components/Timeline/TimelineEventCard.js
@@ -1,11 +1,51 @@
+import React from "react";
 import classes from "./TimelineEventCard.module.css";
 
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 const TimelineEventCard = (props) => {
+  let shortDescription = props.description.slice(0, 60);
+  if (shortDescription.length === 60) shortDescription += "...";
+
+  let date =
+    props.date.getDay() +
+    " " +
+    months[props.date.getMonth()] +
+    " " +
+    props.date.getFullYear();
+  console.log(date, props.date);
   return (
-    <div className={classes.card}>
-      <img className={classes.image} src={props.imgUrl} alt="event" />
-      <div className={classes.content}>Hi</div>
-    </div>
+    <React.Fragment>
+      <div className={classes.overlay}></div>
+      <div className={classes.card}>
+        <img className={classes.image} src={props.imgUrl} alt="event" />
+        <div className={classes.content}>
+          <div className={classes["content__header"]}>
+            <div className={classes["content__title"]}>{props.title}</div>
+            <div className={classes["content__date"]}>{date}</div>
+          </div>
+          <div className={classes["content__description"]}>
+            {shortDescription}
+          </div>
+        </div>
+        <button type="button" className={classes.more}>
+          View more
+        </button>
+      </div>
+    </React.Fragment>
   );
 };
 
